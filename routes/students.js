c337f7cd7e5f303117187035d3b9410a7d3b294b
Sangha Yoon <yoons4@spu.edu>
2022-07-20 16:20:11 -0700

    Completed the step 3

--- routes/students.js
+++ routes/students.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const router = express.Router();
 CourseStudent = require('../models/student');
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
 
 function HandleError(response, reason, message, code){
     console.log('ERROR: ', reason);
     response.status(code || 500).json({"error": message});
 }
 
+app.listen(8003, function(){
+    console.log('Server is running.');
+});
 
+app.get('/', (request, response) => {
+    response.sendFile('input.html');
+});
+
+app.post('/student-courses', function(request, response){
+    var sentence = "A course: " + request.body.studentId + '/n' + request.body.courseDepartment + '/n' +
+    request.body.courseNumber + '/n' + request.body.year + '/n' + request.body.quarter + '/n' + 
+    request.body.credits + '/n' + request.body.grade;
+
+    response.send(sentence);
+});
 router.get('/get', (request, response) => {
     CourseStudent.find().exec((error, student) => {
         if (error){
             HandleError(response, "error retrieving data", "get failed", 500);
         } else {
             response.send(student);
+        }
+    });
+});
+
+router.get('/:id', async(request, response) => {
+    const id = request.params.id;
+    const data = await CourseStudent.findById(id);
+    CourseStudent.find().exec((error, student) => {
+        if (error){
+            HandleError(response, "error retrieving data", "get failed", 500);
+        } else {
+            response.send(data);
         }
     });
 });
@@ -34,7 +64,7 @@ router.post('/post', (request, response) => {
                 credits: studentJSON.credits,
                 grade: studentJSON.grade
             });
-            student.save(() => {
+            student.save((error) => {
                 if (error){
                     response.send({"error": error});
                 } else {
@@ -44,24 +74,26 @@ router.post('/post', (request, response) => {
         }
 });
 
-router.put('/put/:id', (request, response) => {
-    try{
-        const id = request.params.id;
-        const updatedData = request.body;
-        const options = {new: true};
-
-        const result = CourseStudent.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        response.send(result)
+router.put('/put/:id', async(request, response) => {
+    const id = request.params.id;
+    const updatedData = request.body;
+    const options = {new: true};
+    
+    if(!updatedData.studentId || !updatedData.courseDepartment || !updatedData.courseNumber || !updatedData.year || 
+        !updatedData.quarter || !updatedData.credits || !updatedData.grade || updatedData.quarter > 3
+         || updatedData.quarter < 1){
+            HandleError(response, "wrong new information", "post data missing", 500);
+        } else {
+            const result = await CourseStudent.findByIdAndUpdate(
+                id, updatedData, options
+            )
+            response.send(result)
     }
-    catch(error){
-        response.status(400).json({message: error.message})
-    }
-});
+})
 
-router.delete('/delete/:id', (request, response) => {
-
+router.delete('/delete/:id', async(request, response) => {
+    const id = request.params.id;
+    const data = await CourseStudent.findByIdAndDelete(id);
+    response.send("Selected document has been deleted.");
 });
 module.exports = router;
